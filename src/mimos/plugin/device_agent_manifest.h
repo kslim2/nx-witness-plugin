06602a5cf8a225ddb0c8e8f8c8c1aa4216a316c0
kslim2 <lim0709@gmail.com>
2026-02-05 02:05:54 +0800

    feat: device agent manifest

--- src/mimos/plugin/device_agent_manifest.h
+++ src/mimos/plugin/device_agent_manifest.h
@@ -1,3 +1,17 @@
+/**
+ * This is optional but very useful part of Nx analytics plugin, it tells
+ * Nx Witness:
+ * 
+ * - which object types your plugin can produce.
+ * - which attributes (metadata fields) can be attached to each object type.
+ * 
+ * This information is useful in:
+ * - The Rule Engine (when creating rules based on the object/ attributes)
+ * - The search/filter interface
+ * - The timeline/object list display
+ * - Event notification
+ */
+
 #pragma once
 
 #include <string>
@@ -6,20 +20,41 @@ namespace mimos
 {
 namespace plugin
 {
+namespace face_recognition
+{
 
-static const std::string kDeviceAgentManifest = /*suppress newline*/ 1 + (const char*) R"json(
+static const std::string kDeviceAgentManifest = R"json(
 {
     "supportedTypes": [
         {
-            "objectTypeId": "nx.base.Face",
+            "objectTypeId": "nx.base.Person",
             "attributes": [
-                { "name": "Name", "type": "String" },
-                { "name": "Confidence", "type": "Float", "minValue": 0, "maxValue": 1 }
+                "Name",
+                "Similarity",
+                "ListType",
+                "Confidence"
+            ]
+        },
+        {
+            "objectTypeId": "face_recognition.whitelisted",
+            "attributes": [
+                "Name",
+                "Similarity",
+                "Confidence"
+            ]
+        },
+        {
+            "objectTypeId": "face_recognition.blacklisted",
+            "attributes": [
+                "Name",
+                "Similarity",
+                "Confidence"
             ]
         }
     ]
 }
 )json";
 
+} // namespace face_recognition
 } // namespace plugin
 } // namespace mimos
